17581c3e4f1407ff01cb446ab26f2cff7bc33775
Suph Prajamsri <suphprajam95@gmail.com>
2022-02-04 18:12:10 -0800

    added JS to create monthly mortgage line

--- index.js
+++ index.js
@@ -1,13 +1,44 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const initForm = (form) => {
-    const handleSubmit = (event) => {
-      event.preventDefault();
-      console.log("submit", event);
-    };
-    form.addEventListener("submit", handleSubmit);
+// document.addEventListener("DOMContentLoaded", function () {
+//   const initForm = (form) => {
+//     const handleSubmit = (event) => {
+//       event.preventDefault();
+//       console.log("submit", event);
+//     };
+//     form.addEventListener("submit", handleSubmit);
+//   };
+//   const mortgageForm = document.querySelector("#mortgage-form");
+//   if (mortgageForm) {
+//     initForm(mortgageForm);
+//   }
+// });
+
+const initForm = (form) => {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("submit", event);
+
+    // get values in number form for loan, rate, and length
+    const P = parseFloat(document.getElementById("loan").value);
+    const ra = parseFloat(document.getElementById("rate").value);
+    const n = document.getElementById("length").value * 12;
+
+    //equation to get monthyl mortgage
+    const mortgage = P * ((ra * (1 + ra)) ^ (n / ((1 + ra) ^ n) - 1));
+    console.log(P, ra, n);
+    console.log(mortgage);
+
+    //add in monthly mortgage as a new element
+    const newDiv = document.createElement("div");
+    const node = document.createTextNode(
+      "Your monthly mortgage will be $" + mortgage
+    );
+    newDiv.appendChild(node);
+    document.getElementById("mortgage-form").appendChild(newDiv);
   };
-  const mortgageForm = document.querySelector("#mortgage-form");
-  if (mortgageForm) {
-    initForm(mortgageForm);
-  }
-});
+  form.addEventListener("submit", handleSubmit);
+};
+
+const mortgageForm = document.querySelector("#mortgage-form");
+if (mortgageForm) {
+  initForm(mortgageForm);
+}
