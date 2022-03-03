1836fa80ad4a834bf1566f47952ea8acd9852a87
Suph Prajamsri <suphprajam95@gmail.com>
2022-03-02 16:55:40 -0800

    update font, picture, and sizing of form

--- index.js
+++ index.js
@@ -15,7 +15,6 @@
 const initForm = (form) => {
   const handleSubmit = (event) => {
     event.preventDefault();
-    // console.log("submit", event);
 
     // get values in number form for loan, rate, and length
     const p = parseFloat(document.getElementById("loan").value);
@@ -24,24 +23,16 @@ const initForm = (form) => {
 
     //equation to get monthly mortgage
     const mortgage = p * ((m * Math.pow(1 + m, n)) / (Math.pow(1 + m, n) - 1));
-    // console.log(p, m, n);
-    // console.log(Math.pow(1 + m, n));
-    // console.log(mortgage);
-    const roundMortgage = Math.round(mortgage * 100) / 100;
-
-    //add in monthly mortgage as a new element
-    // const newDiv = document.createElement("div");
-    const node = document.createTextNode(roundMortgage);
-    console.log(node);
-    // newDiv.appendChild(node);
-    // document.getElementById("mortgage-form").appendChild(newDiv);
-    document.querySelector(".result").style.visibility = "visible";
-    document.querySelector(".result").appendChild(node);
+    const roundMortgage = mortgage.toFixed(2);
+    //add in monthly mortgage and make result visible
+    document.querySelector(".resultInner__text--bold").innerHTML =
+      ` $` + roundMortgage;
+    document.querySelector(".resultInner").style.visibility = "visible";
   };
   form.addEventListener("submit", handleSubmit);
 };
 
-const mortgageForm = document.querySelector("#mortgage-form");
+const mortgageForm = document.querySelector("#mortgageForm");
 if (mortgageForm) {
   initForm(mortgageForm);
 }
